14cf92b01a57f76df265dc5e689b0d22b931c81b
saravanan <sgoffl1603@gmail.com>
2019-01-01 18:03:48 +0530

    add firebase_post function

--- src/firebase_connect.py
+++ src/firebase_connect.py
@@ -1,2 +1,20 @@
 import auth_keys
-print(auth_keys.firebase_link)
+from datetime import datetime
+from firebase import firebase 
+
+time = datetime.now()
+fb_link = auth_keys.firebase_link
+time =str(time)
+date = time[0:10]
+time = time[11:19]
+print(time)
+def post_onto_fb(status):
+	fb= firebase.FirebaseApplication(fb_link)
+	if status is True:
+		data = {time: "Wallet is present"}
+	else:
+		data= {time:" Wallet is not connected"}
+	
+	fb.patch(fb_link+'wallet_log/'+date, data)
+	
+post_onto_fb(True)
